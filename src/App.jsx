0d3e6ec29d24e03024ff1f8994ff564d25526a1c
iam-kimata <kimataaloyce444@gmail.com>
2026-09-23 16:22:07 +0300

    Implementing Protected Route

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import { Routes, Route } from "react-router-dom";
 import Login from './pages/Auth/Login';
 import Register from './pages/Auth/Register';
+import ProtectedRoute from './components/ProtectedRoute';
 import AdminDashboard from './pages/Admin/Dashboard';
 import Users from './pages/Admin/Users'
 import DoctorDashboard from './pages/Doctor/Dashboard';
@@ -11,9 +12,11 @@ function App() {
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/register" element={<Register />} />
-      <Route path="/dashboard" element={<AdminDashboard />} />
-      <Route path="/users" element={<Users />} />
-      <Route path="/dashboard" element={<DoctorDashboard />} />
+      <Route element={<ProtectedRoute />}>
+        <Route path="/dashboard" element={<AdminDashboard />} />
+        <Route path="/users" element={<Users />} />
+        <Route path="/dashboard" element={<DoctorDashboard />} />
+      </Route>
     </Routes>
   );
 };
